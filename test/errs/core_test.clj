97d0c7dbcafa326cc264231f57b1b80fa438935e
Michel Perez <michel.ingesoft@gmail.com>
2019-07-21 03:44:25 -0500

    wip

--- test/errs/core_test.clj
+++ test/errs/core_test.clj
@@ -6,14 +6,14 @@
   (testing "When an error comes"
     (let [res
           (->
-           [:ok 1]
+           (ok 1)
            (if-ok (try-ok #(ok (/ % 0))))
-           (if-ok (fn [n] [:ok (+ 100 n)])))]
+           (if-ok (fn [n] (ok (+ 100 n)))))]
       (is (= res [:error "Divide by zero"]))))
   (testing "When all ok"
     (let [res
           (->
-           [:ok 1]
+           (ok 1)
            (if-ok (try-ok #(ok (+ % 1))))
-           (if-ok (fn [n] [:ok (+ 100 n)])))]
+           (if-ok (fn [n] (ok (+ 100 n)))))]
       (is (= res [:ok 102])))))
